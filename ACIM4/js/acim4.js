b24027a86bf732dcf3bcb1f9a487ff4461371288
s-kusajima(note) <s-kusajima@sumilab.org>
2016-05-19 14:54:52 +0900

    インデント直して，説明のメモとった

--- ACIM4/js/acim4.js
+++ ACIM4/js/acim4.js
@@ -2,12 +2,15 @@
 // canvas とはブラウザでビットマップイメージを描画するために策定された仕様です．
 // 参考: http://yoppa.org/taumedia10/2065.html
 
+/*global $, console*/
+
 var appInit;
 
-appInit = function(){
+appInit = function () {
     
-    'use strict' 
+    'use strict';
     
+//    drawGridChart: 座標軸を描画　drawGraph: sin波を描画
     var drawGridChart, drawGraph,
         $canvas          = $('#myCanvas'),              // html 上の canvas 要素を id を指定して取得．
         canvasCtx        = $canvas[0].getContext('2d'), // canvas を利用する際はコンテキストを取得する必要があります．             
@@ -16,24 +19,24 @@ appInit = function(){
         col              = 1,  // 座標の x 軸の目盛りの数
         MARGIN           = 15, // 目盛りを描画する際の余白
         AXIS_Y_MARGIN    = 50, // y 軸左側の余白
-        gridProp         = (function(){ // 座標系の描画位置を計算
+        gridProp         = (function () { // 座標系の描画位置を計算
 
             var prop = {
-                    x:{
-                        start :MARGIN,                          // x 軸の始点
-                        end   :canvasCtx.canvas.width - MARGIN, // x 軸の終点
-                        length:null,                            // x 軸の長さ
-                        center:canvasCtx.canvas.height / 2,     // x 軸の高さ (中心位置)
-                        plotPoint:[]                            // x 軸目盛りの x, y 座標．plotPoint[0] には 1つめの目盛りの x, y 座標が格納されている
-                    },
-                    y:{
-                        start :MARGIN,                          // canvas の y 座標は上下が反転していることにご注意を! 
-                                                                // 参考: http://www.html5.jp/canvas/how.html
-                        end   :canvasCtx.canvas.height - MARGIN,
-                        length:null,
-                        center:AXIS_Y_MARGIN,
-                        plotPoint:[]
-                    }
+                x: {
+                    start : MARGIN,                          // x 軸の始点
+                    end   : canvasCtx.canvas.width - MARGIN, // x 軸の終点
+                    length: null,                            // x 軸の長さ
+                    center: canvasCtx.canvas.height / 2,     // x 軸の高さ (中心位置)
+                    plotPoint:[]                             // x 軸目盛りの x, y 座標．plotPoint[0] には 1つめの目盛りの x, y 座標が格納されている
+                },
+                y: {
+                    start : MARGIN,                          // canvas の y 座標は上下が反転していることにご注意を! 
+                                                             // 参考: http://www.html5.jp/canvas/how.html
+                    end   : canvasCtx.canvas.height - MARGIN,
+                    length: null,
+                    center: AXIS_Y_MARGIN,
+                    plotPoint:[]
+                }
             };
 
             prop.x.length = Math.abs(prop.x.end - prop.x.start);
@@ -52,7 +55,7 @@ appInit = function(){
     ;
     
     // gridProp に基づき座標系を描画
-    drawGridChart = function(){
+    drawGridChart = function () {
 
         // canvas を黒色で塗りつぶす．
         canvasCtx.fillStyle = 'rgb(0, 0, 0)';
@@ -82,15 +85,15 @@ appInit = function(){
         canvasCtx.font      = '15px "Arial"';
 
         // x 軸の目盛りを描画．
-        for(var i in gridProp.x.plotPoint){
-            var label;                
+        for (var i in gridProp.x.plotPoint) {
+            var label;
             label = parseInt(i, 10) + 1;
             label = String() + label + 's';
             canvasCtx.fillText(label, gridProp.x.plotPoint[i].x, gridProp.x.plotPoint[i].y);
         }
 
         // y 軸の目盛りを描画．
-        for(var i in gridProp.y.plotPoint){
+        for (var i in gridProp.y.plotPoint) {
             var label;                
             label = parseInt(i, 10) + 1;
             canvasCtx.fillText(label , gridProp.y.plotPoint[i].x, gridProp.y.plotPoint[i].y);  // x 軸より上の y 軸目盛りを描画
@@ -98,13 +101,13 @@ appInit = function(){
     };
 
     // sin 波を描画．    
-    drawGraph = function(frequency, mode){
+    drawGraph = function (frequency, mode) {
 
-        var degree    = 0.0,
-            radian    = 0.0,
+        var degree   = 0.0,
+            radian   = 0.0,
             // サインはを線で描画するために，1つ前の plot 位置を befPlotX, befPlotY に保存しています．
-            befPlotX  = ((degree　/ frequency) / col) + gridProp.y.center,
-            befPlotY  = -Math.sin(radian) *  (gridProp.y.length / 2) + gridProp.x.center
+            befPlotX = ((degree　/ frequency) / col) + gridProp.y.center,
+            befPlotY = -Math.sin(radian) *  (gridProp.y.length / 2) + gridProp.x.center
         ;
 
         drawGridChart();
@@ -115,7 +118,7 @@ appInit = function(){
 
         canvasCtx.beginPath();
 
-        while (radian < Math.PI * 2 * frequency * col){
+        while (radian < Math.PI * 2 * frequency * col) {
 
             var plotX = ((degree　/ frequency) / col) + gridProp.y.center,
                 plotY = -Math.sin(radian) * (gridProp.y.length / 2) + gridProp.x.center
@@ -129,6 +132,11 @@ appInit = function(){
 
             degree += 10;
             radian = degree * Math.PI / ((gridProp.x.length / 2) - MARGIN); // degree を radian に変換
+            
+            canvasCtx.moveTo(plotX, plotY);
+            canvasCtx.lineTo(plotX, (canvasCtx.canvas.height)/2);
+            canvasCtx.stroke();
+            
         }
 
         canvasCtx.closePath();    
