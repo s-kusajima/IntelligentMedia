45b5200a73159359b3e01aa91fc7fb347791415d
s-kusajima(note) <s-kusajima@sumilab.org>
2016-05-30 17:42:22 +0900

    シークバーで内側の円の半径変えられるようにした．色も変えられるようにした．

--- ACIM4/js/acim4.js
+++ ACIM4/js/acim4.js
@@ -11,14 +11,15 @@ appInit = function () {
         canvasCtx        = $canvas[0].getContext('2d'),
         
         startButton      = $('#startButton'),
-        stopButton       = $('#stopButton'),
+        clearButton      = $('#clearButton'),
         
         center           = {x: canvasCtx.canvas.width / 2.0,
                             y: canvasCtx.canvas.height / 2.0},
-        ocr              = canvasCtx.canvas.height / 2.2, // outer cercle radius ///
-        icr              = 100,
-        rad              = 0,
+        ocr              = 180, // outer cercle radius
+        icr              = 100, // inner center radius
+        rad              = 0, // radians
         array            = [],
+        lineColor        = "rgb(255, 0, 0)",
         
         doDraw           = false
     ;
@@ -27,7 +28,6 @@ appInit = function () {
         
         canvasCtx.fillStyle = "rgb(255, 255, 255)";
         canvasCtx.fillRect(0, 0, canvasCtx.canvas.width, canvasCtx.canvas.height);
-        
     }
     
     drawOuterCircle = function () {
@@ -37,7 +37,6 @@ appInit = function () {
         canvasCtx.arc(center.x ,center.y, ocr, 0, 2 * Math.PI, true);
         canvasCtx.stroke();
         canvasCtx.closePath();
-        
     };
     
     drawInnerCircle = function () {
@@ -48,6 +47,7 @@ appInit = function () {
         var icx = center.x + (ocr - icr) * Math.cos(rad / 180 * Math.PI); // inner center x
         var icy = center.y + (ocr - icr) * Math.sin(rad / 180 * Math.PI); // inner center y
         canvasCtx.beginPath();
+        canvasCtx.strokeStyle = "rgb(0, 0, 0)";
         canvasCtx.arc(icx, icy, icr, 0, 2 * Math.PI, true);
         canvasCtx.stroke();
         canvasCtx.closePath();
@@ -55,15 +55,29 @@ appInit = function () {
         var px = icx + icr * Math.cos((ocr - icr) / icr * rad / 180 * Math.PI); // point x
         var py = icy - icr * Math.sin((ocr - icr) / icr * rad / 180 * Math.PI); // point y
         
+        var c = $('input[name=color]:checked').val();
+        switch(c){
+            case "red":
+                lineColor = "rgb(255, 0, 0)";
+                break;
+            case "green":
+                lineColor = "rgb(0, 255, 0)";
+                break;
+            case "blue":
+                lineColor = "rgb(0, 0, 255)";
+                break;
+        }
+        
         canvasCtx.beginPath();
-        canvasCtx.strokeStyle = "rgb(255, 0, 0)";
+        canvasCtx.strokeStyle = lineColor;
         canvasCtx.arc(px, py, 5, 0, 2 * Math.PI, true);
         canvasCtx.stroke();
         canvasCtx.closePath();
         
-        var obj = {"x":px, "y":py};
+        var obj = {"x":px, "y":py, "r":icr, "c":lineColor};
         array.push(obj);
         
+        /*
         canvasCtx.beginPath();
         canvasCtx.strokeStyle = "rgb(255, 0, 0)";
         canvasCtx.moveTo(array[0].x, array[0].y);
@@ -71,45 +85,65 @@ appInit = function () {
             canvasCtx.lineTo(array[i].x, array[i].y);
         }
         canvasCtx.stroke();
+        */
         
-        rad+=10;
+        for(var i = 1; i < array.length; i++){
+            
+            if(array[i - 1].r != array[i].r) continue;
+            canvasCtx.beginPath();
+            canvasCtx.strokeStyle = array[i].c;
+            canvasCtx.moveTo(array[i - 1].x, array[i - 1].y);
+            canvasCtx.lineTo(array[i].x, array[i].y);
+            canvasCtx.stroke();
+            canvasCtx.closePath();
+        }
         
+        rad += 5;
     };
     
     startButton.click(function () {
        
-        if(!doDraw){
+        if(!doDraw){   
             doDraw = true;
-            console.log("pushed startbutton");
+            startButton.text("描画停止");
+        }else{
+            doDraw = false;
+            startButton.text("描画開始");
         }
-        
     });
     
-    stopButton.click(function () {
-       
+    clearButton.click(function () {
+        
         if(doDraw){
             doDraw = false;
-            console.log("pushed stopbutton");
+            startButton.text("描画停止");
         }
+        
+        rad = 0.0;
+        array = [];
+                
+        drawInnerCircle();
     });
 
     var ddd = setInterval(function(){
         if(doDraw) drawInnerCircle();
-    }, 50);
+    }, 20);
     
     (function init(){
+        
         
         var frequencySlider  = $('#frequencySlider'),
             frequencyValue   = $('#frequencyValue')
         ;
-
-        clearCanvas();
-        drawOuterCircle();
+        
         drawInnerCircle();
-        frequencySlider.on('change mousemove mousedown', function(){
-//            drawGraph($(this).val(), 0);
-//            frequencyValue.text(String() + ($(this).val() * col) + 'Hz');
+        frequencySlider.on('change', function(){
+            frequencyValue.text(String() + $(this).val());
+            icr = $(this).val();
+            rad = 0.0;
+            drawInnerCircle();
         });
+        
     })();
 };
 
